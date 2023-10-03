4fa1dc41654db0bd238ba2968204dcd1883e8a48
Nahlakhalifaa <nahla.kh0025@gmail.com>
2023-10-03 10:59:42 -0500

    Refactoring to dry the code.

---
+++ src/main/java/org/launchcode/techjobs/oo/JobField.java
@@ -0,0 +1,48 @@
+package org.launchcode.techjobs.oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    protected int id;
+    protected static int nextId = 1;
+    protected String value;
+
+    @Override
+    public String toString() {
+        return value;
+    }
+    public JobField() {
+        id = nextId;
+        nextId++;
+    }
+
+    public JobField(String value) {
+        this();
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof JobField jobField)) return false;
+        return getId() == jobField.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
